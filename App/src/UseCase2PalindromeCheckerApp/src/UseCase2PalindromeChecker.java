cafc9ad919ee78800cefabe579750bee2ae1677f
smirta07 <smirta.pathak@gmail.com>
2026-03-16 15:08:17 +0530

    Update UseCase2PalindromeChecker.java

--- App/src/UseCase2PalindromeCheckerApp/src/UseCase2PalindromeChecker.java
+++ App/src/UseCase2PalindromeCheckerApp/src/UseCase2PalindromeChecker.java
@@ -1,3 +1,13 @@
 public class UseCase2PalindromeChecker {
+  public static void main(String[] args) {
+        String word = "madam";
+        String reversed = new StringBuilder(word).reverse().toString();
+
+        if (word.equals(reversed)) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is not a Palindrome");
+        }
+    }
 
 }
