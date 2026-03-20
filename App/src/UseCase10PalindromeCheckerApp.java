ba60a2d1991a6f3245aa3598268af7bedd9e6432
smirta07 <smirta.pathak@gmail.com>
2026-03-20 23:07:31 +0530

    UC10:Case sensitive palindrome

---
+++ App/src/UseCase10PalindromeCheckerApp.java
@@ -0,0 +1,31 @@
+public class UseCase10PalindromeCheckerApp {
+    static boolean isPalindrome(String input) {
+
+        // Normalize string: remove spaces and convert to lowercase
+        String normalized = input.replaceAll("\\s+", "").toLowerCase();
+
+        int start = 0;
+        int end = normalized.length() - 1;
+
+        // Compare characters
+        while (start < end) {
+            if (normalized.charAt(start) != normalized.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        String input = "a man a plan a canal panama";
+
+        boolean result = isPalindrome(input);
+
+        System.out.println("Input: " + input);
+        System.out.println("Is Palindrome? : " + result);
+    }
+}
